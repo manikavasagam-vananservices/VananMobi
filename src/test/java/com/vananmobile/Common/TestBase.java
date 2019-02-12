d1937c7ee5265fcb3da8543e4473346bc7a7f478
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-02-12 21:08:06 +0530

    modified code

--- src/test/java/com/vananmobile/Common/TestBase.java
+++ src/test/java/com/vananmobile/Common/TestBase.java
@@ -45,7 +45,7 @@ public class TestBase {
     private String  platformVersion = "7.0";
     private String deviceName = "33001f8921b473eb";
     private String  bName = "chrome";
-    private String  appiumUrl = "http://0.0.0.0:4724/wd/hub";
+    private String  appiumUrl = "http://127.0.0.1:4724/wd/hub";
 
     private void getConfiguration() {
 
@@ -70,9 +70,9 @@ public class TestBase {
     private void initDriver() {
         try {
             if (platform.equals("Android")) {
-                driver = new AndroidDriver(new URL("http://0.0.0.0:4724/wd/hub"), capabilities);
+                driver = new AndroidDriver(new URL(appiumUrl), capabilities);
             } else if (platform.equals("ios")) {
-                driver = new IOSDriver(new URL("http://0.0.0.0:4724/wd/hub"), capabilities);
+                driver = new IOSDriver(new URL(appiumUrl), capabilities);
             }
             driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
         } catch (MalformedURLException ex) {
